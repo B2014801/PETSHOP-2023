c10b4905a25c6b67526b3c4882e78c03341434e0
B2014801 <trungb2014801@student.ctu.edu.vn>
2023-09-17 10:33:05 +0700

    Revenue done

--- admin/src/views/Revenue.vue
+++ admin/src/views/Revenue.vue
@@ -1,5 +1,10 @@
 <template>
-    <v-chart v-if="dataLoaded" class="chart" :option="option" autoresize />
+    <div style="background-color: #100c2a" class="text-end p-2">
+        <select name="" id="" v-model="selectedYear" @change="updateChart">
+            <option :value="year" v-for="year in resultYear">{{ year }}</option>
+        </select>
+    </div>
+    <v-chart v-if="dataLoaded" class="chart" :option="option" :autoresize="true" :key="autoresizeKey" />
 </template>
 
 <script setup>
@@ -8,15 +13,23 @@ import { CanvasRenderer } from 'echarts/renderers';
 import { BarChart } from 'echarts/charts';
 import { TitleComponent, TooltipComponent, LegendComponent, GridComponent } from 'echarts/components';
 import VChart, { THEME_KEY } from 'vue-echarts';
-import { ref, provide, onMounted } from 'vue';
+import { ref, provide, onMounted, watch } from 'vue';
 
 import InvoiceService from '@/services/invoice.service';
 
 use([CanvasRenderer, BarChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent]);
 
-provide(THEME_KEY, 'white');
+provide(THEME_KEY, 'dark');
 let month = Array(12).fill(0);
-const option = ref({
+const dataLoaded = ref(false);
+let maxyear = 0; //Math.max(array)
+let minyear = Infinity; //Math.max(array)
+let dateNow = new Date();
+let selectedYear = ref(dateNow.getFullYear());
+watch(selectedYear, (newVal, oldVal) => {
+    getRevenue();
+});
+let option = ref({
     title: {
         text: 'Doanh thu',
         left: 'center',
@@ -61,30 +74,56 @@ const option = ref({
         },
     ],
 });
-const dataLoaded = ref(false);
 
 const getRevenue = async () => {
     try {
         const revenue = await InvoiceService.getRevenue();
-        let year = 2023;
-        revenue.map((item) => {
-            if (item._id.year == year) {
-                const _month = parseInt(item._id.month);
 
+        if (revenue) {
+            autoresizeKey++;
+            getMaxAndMin(revenue);
+        }
+        month = Array(12).fill(0);
+        await revenue.map((item) => {
+            if (item._id.year == parseInt(selectedYear._value)) {
+                const _month = parseInt(item._id.month);
                 month[_month - 1] = parseInt(item.totalRevenue);
             }
         });
+        // maxyear = Math.max(...revenue._id.year);
+        option.value.series[0].data = month;
+        ++autoresizeKey;
         dataLoaded.value = true;
-    } catch (error) {}
+    } catch (error) {
+        console.log(error);
+    }
 };
+let resultYear = [];
+const getMaxAndMin = (array) => {
+    array.map((item) => {
+        if (item._id.year > maxyear) {
+            maxyear = item._id.year;
+        }
+        if (item._id.year < minyear) {
+            minyear = item._id.year;
+        }
+        resultYear = [];
+
+        for (let i = minyear; i <= maxyear; i++) {
+            resultYear.push(i);
+        }
+    });
+};
+let autoresizeKey = 1;
 
 onMounted(() => {
+    autoresizeKey++;
     getRevenue();
 });
 </script>
 
 <style scoped lang="scss">
 .chart {
-    height: 88vh;
+    height: 82vh;
 }
 </style>
